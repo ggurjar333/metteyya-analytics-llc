850e6597e1c310086416d3af07fd288178b348c3
Wesley-waka <wesleywaka77@gmail.com>
2023-10-21 20:42:00 +0300

    client logo section

---
+++ src/Components/ClientsLogo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ClientsLogo = () => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default ClientsLogo;
